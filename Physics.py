8481bdd6b6002de7971724466d9ef5219431fcf0
ArtemEvstafev <vur1artem1cheb@gmail.com>
2021-03-09 13:52:56 +0300

    Лаба 2.4.1

--- Physics.py
+++ Physics.py
@@ -7,13 +7,15 @@ T = np.array([33, 32, 31.2, 30.2, 29.2, 28.1, 27.3, 26.1, 25.2, 24.2, 23, 22, 21
 P2 = np.array([3.84, 3.64, 3.51, 3.32, 2.95, 2.92, 2.8, 2.7, 2.55, 2.35, 2.23, 2.13, 2.02, 1.9])
 P = ((P1 + P2) / 2) * 10 * 133.332
 T+=273
-x = 1/T
-y = np.log(P)
-
-file = open('PLAYERS.txt', 'w')
-for i in range(0,14):
-    print(x[i], file=file)
-
+#x = 1/T
+#y = np.log(P)
+x = T
+print(x)
+y = P
+#file = open('PLAYERS.txt', 'w')
+#for i in range(0,14):
+#    print(x[i], file=file)
 #file.close()
-#plt.plot(x, y)
-#plt.show()
+
+plt.plot(x, y)
+plt.show()
