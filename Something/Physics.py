249bf53f5814e37901944421c84bffec00402ab8
ArtemEvstafev <vur1artem1cheb@gmail.com>
2021-09-01 23:33:52 +0300

    dfg

--- Something/Physics.py
+++ Something/Physics.py
@@ -1,6 +1,7 @@
+'''
 import numpy as np
 import matplotlib.pyplot as plt
-'''
+
 #f2 = np.array(list(map(float, input().split())))#берет вводимые значения и делает из них вектор
 f1 = np.array([827, 996, 1172, 1496, 2165, 2357])
 f2 = np.array([838, 988, 1166, 1497, 2157, 2338])
@@ -11,9 +12,8 @@ y = f
 print(f)
 #file = open('PLAYERS.txt', 'w')
 #for i in range(0,14):
-#    print(x[i], file=file)
+#print(x[i], file=file)
 #file.close()
-
 plt.plot(x, y)
 plt.show()
 '''
@@ -28,5 +28,5 @@ t = 0.1
 Y = m*(2*L*B)**2/(R*T)
 y = Y*((t/T)**2 + (2*l/L)**2 + (2*b/B)**2)**0.5
 print(Y, y, y/Y*100)
-f1 = np.array([1501, 2354, 2573, 2786])
-print(f1-655)
+#f1 = np.array([1501, 2354, 2573, 2786])
+#print(f1-655)
